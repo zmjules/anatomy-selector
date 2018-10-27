d543b844cd2da923df634a26eb3ffa535e23cbf0
zmjules <julia.zochodne@gmail.com>
2018-10-26 22:37:29 -0600

    Removes unused attributes

--- BodyStructure.py
+++ BodyStructure.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
 from anytree import Node, RenderTree
-	
+
+# Create tree structure for data	
 def createBodyStructure():
-	chest = Node("Chest", id=0, selected=False)
-	lungs = Node("Lungs", id=1, parent=chest, selected=False)
-	rLung = Node("Right Lung", id=2, parent=lungs, selected=False)
-	lLung = Node("Left Lung", id=3, parent=lungs, selected=False)
-	rSupLobe = Node("Superior Lobe", id=4, parent=rLung, selected=False)
-	midLobe = Node("Middle Lobe", id=5, parent=rLung, selected=False)
-	rInfLobe = Node("Inferior Lobe", id=6, parent=rLung, selected=False)
-	lSupLobe = Node("Superior Lobe", id=7, parent=lLung, selected=False)
-	rInfLobe = Node("Inferior Lobe", id=8, parent=lLung, selected=False)
-	heart = Node("Heart", id=9, parent=chest, selected=False)
-	lventricle = Node("Left Ventricle", id=10, parent=heart, selected=False)
-	rventricle = Node("Right Ventricle", id=11, parent=heart, selected=False)
-	latrium = Node("Left Atrium", id=12, parent=heart, selected=False)
-	ratrium = Node("Right Atrium", id=13, parent=heart, selected=False)
-	septum = Node("Septum", id=14, parent = heart, selected=False)
-	#print(RenderTree(chest))
+	chest = Node("Chest", id=0)
+	lungs = Node("Lungs", id=1, parent=chest)
+	rLung = Node("Right Lung", id=2, parent=lungs)
+	lLung = Node("Left Lung", id=3, parent=lungs)
+	rSupLobe = Node("Superior Lobe", id=4, parent=rLung)
+	midLobe = Node("Middle Lobe", id=5, parent=rLung)
+	rInfLobe = Node("Inferior Lobe", id=6, parent=rLung)
+	lSupLobe = Node("Superior Lobe", id=7, parent=lLung)
+	rInfLobe = Node("Inferior Lobe", id=8, parent=lLung)
+	heart = Node("Heart", id=9, parent=chest)
+	lventricle = Node("Left Ventricle", id=10, parent=heart)
+	rventricle = Node("Right Ventricle", id=11, parent=heart)
+	latrium = Node("Left Atrium", id=12, parent=heart)
+	ratrium = Node("Right Atrium", id=13, parent=heart)
+	septum = Node("Septum", id=14, parent = heart)
 	return chest; 
